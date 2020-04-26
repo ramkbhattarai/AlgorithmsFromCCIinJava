5a73a0a4e48617e8439200efbb7624c3fd31651b
Ram <ramkb466@gmail.com>
2020-04-26 12:49:50 -0400

    changed the variables name

--- CCIProblems/src/ArraysAndString/IsUnique.java
+++ CCIProblems/src/ArraysAndString/IsUnique.java
@@ -13,15 +13,15 @@ public class IsUnique {
 static boolean isUniqueChars(String s) {
 	if (s.length() > 128) return false;
 	
-	boolean[] char_set= new boolean[128];
+	boolean[] setOfChar= new boolean[128];
 	
 	for (int i= 0; i < s.length(); i++) {
 		
 		int value= s.charAt(i);
-		if (char_set[value]) {
+		if (setOfChar[value]) {
 		return false;
 		}
-		char_set[value]= true;
+		setOfChar[value]= true;
 	}
 	
 	
