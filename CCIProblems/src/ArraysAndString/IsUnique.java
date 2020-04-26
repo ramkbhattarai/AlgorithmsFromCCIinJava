2b7423122c5a70a28144a3dee07a13e53d468e02
Ram <ramkb466@gmail.com>
2020-04-26 10:50:01 -0400

    solved is unique string problem

--- CCIProblems/src/ArraysAndString/IsUnique.java
+++ CCIProblems/src/ArraysAndString/IsUnique.java
@@ -27,10 +27,22 @@ static boolean isUniqueChars(String str) {
 	
 	return true;
 }
+
+static boolean isUniqueChars2(String str) {
+	int checker= 0;
+	for (int i= 0; i < str.length(); i++) {
+		int val= str.charAt(i) - 'a';
+		if ((checker & (1 << val)) > 0) {
+			return false;
+		}
+		checker |= (1 << val);
+	}
+	return true;
+}
 	
 	public static void main(String[] args) {
 		System.out.println(isUniqueChars("hi there"));
-
+		System.out.println(isUniqueChars2("hi there"));
 	}
 
 }
