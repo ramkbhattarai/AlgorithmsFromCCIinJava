0c35ccd52d6124d83aac6c132c054fb24d248cf1
Ram <ramkb466@gmail.com>
2020-05-11 07:45:40 -0400

    2nd approach

--- CCIProblems/src/ArraysAndString/StringCompression.java
+++ CCIProblems/src/ArraysAndString/StringCompression.java
@@ -25,5 +25,21 @@ public class StringCompression {
 		
 		}
 		return compressedString.length() < str.length() ? compressedString : str;
-	}	
+	}
+	// approach 2
+	
+	String compress(String str) {
+		StringBuilder compressed= new StringBuilder();
+		int countConsecutive= 0;
+		for (int i= 0; i < str.length(); i++) {
+		countConsecutive++;
+		
+		if (i + 1 >= str.length()|| str.charAt(i) != str.charAt(i + 1)) {
+			compressed.append(str.charAt(i)); 
+			compressed.append(countConsecutive);
+		countConsecutive= 0;
+		}
+		}
+		return compressed.length() < str.length() ? compressed.toString() : str;
+	}
 }
